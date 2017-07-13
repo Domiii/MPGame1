bf1753217dcaa9b5da77f6dd6a375584ca2cf73b
Domiii <dominik.seifert@gmail.com>
2017-07-13 18:23:53 +0800

    - minor update

---
+++ Assets/Scripts/Unit.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unit : MonoBehaviour {
+	public float health = 100;
+	public float maxHealth = 100;
+
+	void Start () {
+		
+	}
+
+	void Update () {
+		
+	}
+}
